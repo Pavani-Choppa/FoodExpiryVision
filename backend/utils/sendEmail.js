8ed5e670a48b8fed4a50e2471b433dd07c4dbbee
Pavani-Choppa <pavanicm011@gmail.com>
2026-04-02 13:39:49 +0530

    resend done

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,18 +1,32 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
+
+// export const sendEmail = async (to, subject, text) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   await transporter.sendMail({
+//     from: `"FoodExpiryVision" <${process.env.EMAIL_USER}>`,
+//     to,
+//     subject,
+//     text,
+//   });
+// };  
+
+
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  await transporter.sendMail({
-    from: `"FoodExpiryVision" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    text,
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: to,
+    subject: subject,
+    text: text,
   });
 };
